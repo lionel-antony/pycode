167ec4fbd14c394170c822f0c98df4f12217fb89
Lionel Antony <lionel.antony@gmail.com>
2020-03-07 21:51:30 +0530

    upd

---
+++ name01.py
@@ -0,0 +1,3 @@
+first_name = 'Lionel'
+last_name = 'Antony'
+print('Hello ' + first_name + ' ' + last_name)
---
+++ string_use_01.py
@@ -0,0 +1,5 @@
+sentence = 'The bird is kingfisher'
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count('i'))
